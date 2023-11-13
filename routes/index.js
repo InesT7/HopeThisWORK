23f1ba70ef0ee96409e08724d4f1300754881776
InesT7 <mitorres@gus.pittstate.edu>
2023-11-13 00:37:48 -0600

    done

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'profile' });
+});
+
+
 module.exports = router;
